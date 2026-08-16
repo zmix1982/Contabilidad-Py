2a4c922dad8620bfcf5bf23e34bd77a80ee09380
zmix1982 <lzazzi@gmail.com>
2026-08-15 21:50:16 -0300

    Actualizar login.php

--- loguin.php
+++ loguin.php
@@ -133,4 +133,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</html>.
